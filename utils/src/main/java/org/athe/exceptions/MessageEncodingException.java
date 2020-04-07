31a9cf24b1846fd75e0db67db132611826e89f7e
hnseud2020 <63275890+hnseud2020@users.noreply.github.com>
2020-04-07 17:02:23 +0800

    Add files via upload

---
+++ utils/src/main/java/org/athe/exceptions/MessageEncodingException.java
@@ -0,0 +1,10 @@
+package org.athe.exceptions;
+
+/**
+ * Encoding exception.
+ */
+public class MessageEncodingException extends RuntimeException {
+    public MessageEncodingException(String message) {
+        super(message);
+    }
+}
